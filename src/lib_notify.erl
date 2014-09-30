7d630b91506c024d63028550ae09e41c17b81fc8
Matthew Branton <mbranton@emberfinancial.com>
2014-09-29 22:31:23 -0400

    added lib_notify

---
+++ src/lib_notify.erl
@@ -0,0 +1,44 @@
+%
+% [Ember Financial] INC ("COMPANY") CONFIDENTIAL
+% Unpublished Copyright (c) 2014-2015 [Ember Financial, Inc], All Rights Reserved.
+%
+% NOTICE:  All information contained herein is, and remains the property of
+% COMPANY. The intellectual and technical concepts contained herein are 
+% proprietary to COMPANY and may be covered by U.S. and Foreign Patents, 
+% patents in process, and are protected by trade secret or copyright law.
+% Dissemination of this information or reproduction of this material is 
+% strictly forbidden unless prior written permission is obtained from COMPANY.
+% Access to the source code contained herein is hereby forbidden to anyone 
+% except current COMPANY employees, managers or contractors who have executed 
+% Confidentiality and Non-disclosure agreements explicitly covering such access.
+%
+% The copyright notice above does not evidence any actual or intended publication
+% or disclosure  of  this source code, which includes information that is
+% confidential and/or proprietary, and is a trade secret, of  COMPANY.  
+% ANY REPRODUCTION, MODIFICATION, DISTRIBUTION, PUBLIC  PERFORMANCE, OR PUBLIC
+% DISPLAY OF OR THROUGH USE  OF THIS  SOURCE CODE  WITHOUT  THE EXPRESS WRITTEN
+% CONSENT OF COMPANY IS STRICTLY PROHIBITED, AND IN VIOLATION OF APPLICABLE 
+% LAWS AND INTERNATIONAL TREATIES.  THE RECEIPT OR POSSESSION OF  THIS SOURCE
+% CODE AND/OR RELATED INFORMATION DOES NOT CONVEY OR IMPLY ANY RIGHTS TO
+% REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS, OR TO MANUFACTURE, USE, OR
+% SELL ANYTHING THAT IT  MAY DESCRIBE, IN WHOLE OR IN PART.                
+%
+
+-module(lib_notify).
+-author('mbranton@emberfinancial.com').
+
+-export([connect_nodes/1]).
+
+%%% Connect functions
+
+connect_nodes(undefined) -> ok;
+connect_nodes(N) when is_atom(N) -> connect_node(N);
+connect_nodes([]) -> ok;
+connect_nodes(NodeList) when is_list(NodeList) ->
+	[H|T] = NodeList,
+	connect_node(H),
+	connect_nodes(T).
+
+connect_node(Node) ->
+	true = net_kernel:connect_node(Node).
+
