09707116df37f3cb879fde93cc0dcf385489e9b7
Matt Branton <mbranton@emberfinancial.com>
2015-08-04 22:28:17 -0400

    testing utilities

---
+++ src/lib_utils.erl
@@ -0,0 +1,60 @@
+%
+% [Ember Financial] INC ("COMPANY") CONFIDENTIAL
+% Unpublished Copyright (c) 2014-2015 [Ember Financial, Inc], All Rights Reserved.
+%
+% NOTICE:  All information contained herein is, and remains the property of
+% COMPANY. The intellectual and technical concepts contained herein are 
+% proprietary to COMPANY and may be covered by U.S. and Foreign Patents, 
+% patents in process, and are protected by trade secret or copyright law.
+% Dissemination of this information or reproduction of this material is 
+% strictly forbidden unless prior written permission is obtained from COMPANY.
+% Access to the source code contained herein is hereby forbidden to anyone 
+% except current COMPANY employees, managers or contractors who have executed 
+% Confidentiality and Non-disclosure agreements explicitly covering such access.
+%
+% The copyright notice above does not evidence any actual or intended publication
+% or disclosure  of  this source code, which includes information that is
+% confidential and/or proprietary, and is a trade secret, of  COMPANY.  
+% ANY REPRODUCTION, MODIFICATION, DISTRIBUTION, PUBLIC  PERFORMANCE, OR PUBLIC
+% DISPLAY OF OR THROUGH USE  OF THIS  SOURCE CODE  WITHOUT  THE EXPRESS WRITTEN
+% CONSENT OF COMPANY IS STRICTLY PROHIBITED, AND IN VIOLATION OF APPLICABLE 
+% LAWS AND INTERNATIONAL TREATIES.  THE RECEIPT OR POSSESSION OF  THIS SOURCE
+% CODE AND/OR RELATED INFORMATION DOES NOT CONVEY OR IMPLY ANY RIGHTS TO
+% REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS, OR TO MANUFACTURE, USE, OR
+% SELL ANYTHING THAT IT  MAY DESCRIBE, IN WHOLE OR IN PART.                
+%
+%
+
+%% Miscelaneous compound functions useful for testing
+
+-module(lib_utils).
+-author('mbranton@emberfinancial.com').
+
+-export([block/1,
+		 metablock/1,
+		 info/1]).
+
+-include_lib("bitter.hrl").
+
+
+block(Height) when is_integer(Height) ->
+	block(element(2, bitter_chaind:blockheight(near, Height)));
+
+block(Hash) when is_binary(Hash) ->
+	{A, _Meta} = metablock(Hash),
+	A.
+
+metablock(Height) when is_integer(Height) ->
+	metablock(element(2, bitter_chaind:blockheight(near, Height)));
+metablock(Hash) when is_binary(Hash) ->
+	{ok, A, Meta} = bitter_blockd_app:query(near, colorhash, Hash),
+	{A, Meta}.
+
+info(B) when is_record(B, bbdef) ->
+	#{addresses => lib_block:addresses(B),
+	  colors => lib_block:colors(B),
+	  txcount => length(B#bbdef.txdata)};
+info(Criteria) -> info(block(Criteria)).
+
+	
+
