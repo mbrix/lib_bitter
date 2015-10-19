b165a3ddcc69e33ed7d41f99e53ea3c659b0772a
Matt Branton <mbranton@emberfinancial.com>
2015-10-18 22:19:30 -0400

    switched to aligned binary parsing and added more sophisticated error scanning in munged files

--- src/lib_parse.erl
+++ src/lib_parse.erl
@@ -80,7 +80,7 @@ parse_tx(TxData) ->
 	T.
 
 extractLoop(Data, StartOffset) ->
-	try
+%	try
 	    case extract(Data) of
 	    	{ok, Block2, TxOffsets, Next} ->
 	    	    BlockSize = byte_size(Data) - byte_size(Next),
@@ -94,10 +94,10 @@ extractLoop(Data, StartOffset) ->
 	    	ok -> 
 	    	    erlang:garbage_collect(self()),
 	    	    done
-	    end
-	catch
-		_:_ -> done
-	end.
+	    end.
+%	catch
+%		_:_ -> done
+%	end.
 
 adjust_offsets(TxOffsets, Adjustment) ->
     lists:map(fun(E) ->
@@ -109,57 +109,57 @@ norm(?OP_0) -> 0;
 norm(OP_X) -> OP_X - 80.
 
 % Script classification
-parse_script(<<?OP_DUP:8, ?OP_HASH160:8, 16#14:8, Pubkey:160/bitstring, ?OP_EQUALVERIFY:8, ?OP_CHECKSIG:8>>) ->
+parse_script(<<?OP_DUP:8, ?OP_HASH160:8, 16#14:8, Pubkey:20/binary, ?OP_EQUALVERIFY:8, ?OP_CHECKSIG:8>>) ->
 	{p2pkh, Pubkey};
-parse_script(<<?OP_HASH160:8, 16#14:8, Hash:160/bitstring, ?OP_EQUAL:8>>) -> {p2sh, Hash};
+parse_script(<<?OP_HASH160:8, 16#14:8, Hash:20/binary, ?OP_EQUAL:8>>) -> {p2sh, Hash};
 % P2SH classification
-parse_script(<<?OP_HASH160:8, Hash:160/bitstring, ?OP_EQUAL:8>>) -> {p2sh, Hash};
+parse_script(<<?OP_HASH160:8, Hash:20/binary, ?OP_EQUAL:8>>) -> {p2sh, Hash};
 % Less common types
-parse_script(<<?OP_SHA256:8, Pubkey:264/bitstring, ?OP_EQUAL:8>>) -> {p2pkh2, Pubkey};
-parse_script(<<65:8, Pubkey:520/bitstring, ?OP_CHECKSIG:8>>) -> {full_pubkey, Pubkey};
-parse_script(<<33:8, Pubkey:264/bitstring, ?OP_CHECKSIG:8>>) -> {compressed_pubkey, Pubkey};
-parse_script(<<32:8, Pubkey:256>>) -> {malformed, Pubkey};
+parse_script(<<?OP_SHA256:8, Pubkey:33/binary, ?OP_EQUAL:8>>) -> {p2pkh2, Pubkey};
+parse_script(<<65:8, Pubkey:65/binary, ?OP_CHECKSIG:8>>) -> {full_pubkey, Pubkey};
+parse_script(<<33:8, Pubkey:33/binary, ?OP_CHECKSIG:8>>) -> {compressed_pubkey, Pubkey};
+parse_script(<<32:8, Pubkey:32>>) -> {malformed, Pubkey};
 
 % Common Multisig parsing
 % N of 1 ??
-parse_script(<<M:8, 33:8, _:8, Pubkey:256/bitstring, ?OP_1:8, ?OP_CHECKMULTISIG>>) ->
+parse_script(<<M:8, 33:8, _:8, Pubkey:32/binary, ?OP_1:8, ?OP_CHECKMULTISIG>>) ->
 	{multisig, {{norm(M),1}, [Pubkey]}};
-parse_script(<<M:8, 65:8, _:8, Pubkey:512/bitstring, ?OP_1:8, ?OP_CHECKMULTISIG>>) ->
+parse_script(<<M:8, 65:8, _:8, Pubkey:64/binary, ?OP_1:8, ?OP_CHECKMULTISIG>>) ->
 	{multisig, {{norm(M),1}, [Pubkey]}};
 
 % N of 2
-parse_script(<<M:8, 33:8, Pubkey:264/bitstring, 33:8, Pubkey2:264/bitstring, ?OP_2:8, ?OP_CHECKMULTISIG>>) ->
+parse_script(<<M:8, 33:8, Pubkey:33/binary, 33:8, Pubkey2:33/binary, ?OP_2:8, ?OP_CHECKMULTISIG>>) ->
 	{multisig, {{norm(M),2}, [Pubkey, Pubkey2]}};
-parse_script(<<M:8, 33:8, Pubkey:264/bitstring, 65:8, Pubkey2:520/bitstring, ?OP_2:8, ?OP_CHECKMULTISIG>>) ->
+parse_script(<<M:8, 33:8, Pubkey:33/binary, 65:8, Pubkey2:65/binary, ?OP_2:8, ?OP_CHECKMULTISIG>>) ->
 	{multisig, {{norm(M),2}, [Pubkey, Pubkey2]}};
-parse_script(<<M:8, 65:8, Pubkey:520/bitstring, 33:8, Pubkey2:264/bitstring, ?OP_2:8, ?OP_CHECKMULTISIG>>) ->
+parse_script(<<M:8, 65:8, Pubkey:65/binary, 33:8, Pubkey2:33/binary, ?OP_2:8, ?OP_CHECKMULTISIG>>) ->
 	{multisig, {{norm(M),2}, [Pubkey, Pubkey2]}};
-parse_script(<<M:8, 65:8, Pubkey:520/bitstring, 65:8, Pubkey2:520/bitstring, ?OP_2:8, ?OP_CHECKMULTISIG>>) ->
+parse_script(<<M:8, 65:8, Pubkey:65/binary, 65:8, Pubkey2:65/binary, ?OP_2:8, ?OP_CHECKMULTISIG>>) ->
 	{multisig, {{norm(M),2}, [Pubkey, Pubkey2]}};
 
 % 1 of 3
-parse_script(<<M:8, 33:8, Pubkey:264/bitstring, 33:8, Pubkey2:264/bitstring, 33:8, Pubkey3:264/bitstring, ?OP_3:8, ?OP_CHECKMULTISIG>>) ->
+parse_script(<<M:8, 33:8, Pubkey:33/binary, 33:8, Pubkey2:33/binary, 33:8, Pubkey3:33/binary, ?OP_3:8, ?OP_CHECKMULTISIG>>) ->
 	{multisig, {{norm(M),3}, [Pubkey, Pubkey2, Pubkey3]}};
 
-parse_script(<<M:8, 33:8, Pubkey:264/bitstring, 33:8, Pubkey2:264/bitstring, 65:8, Pubkey3:520/bitstring, ?OP_3:8, ?OP_CHECKMULTISIG>>) ->
+parse_script(<<M:8, 33:8, Pubkey:33/binary, 33:8, Pubkey2:33/binary, 65:8, Pubkey3:65/binary, ?OP_3:8, ?OP_CHECKMULTISIG>>) ->
 	{multisig, {{norm(M),3}, [Pubkey, Pubkey2, Pubkey3]}};
 
-parse_script(<<M:8, 33:8, Pubkey:264/bitstring, 65:8, Pubkey2:520/bitstring, 33:8, Pubkey3:264/bitstring, ?OP_3:8, ?OP_CHECKMULTISIG>>) ->
+parse_script(<<M:8, 33:8, Pubkey:33/binary, 65:8, Pubkey2:65/binary, 33:8, Pubkey3:33/binary, ?OP_3:8, ?OP_CHECKMULTISIG>>) ->
 	{multisig, {{norm(M),3}, [Pubkey, Pubkey2, Pubkey3]}};
 
-parse_script(<<M:8, 65:8, Pubkey:520/bitstring, 33:8, Pubkey2:264/bitstring, 33:8, Pubkey3:264/bitstring, ?OP_3:8, ?OP_CHECKMULTISIG>>) ->
+parse_script(<<M:8, 65:8, Pubkey:65/binary, 33:8, Pubkey2:33/binary, 33:8, Pubkey3:33/binary, ?OP_3:8, ?OP_CHECKMULTISIG>>) ->
 	{multisig, {{norm(M),3}, [Pubkey, Pubkey2, Pubkey3]}};
 
-parse_script(<<M:8, 65:8, Pubkey:520/bitstring, 33:8, Pubkey2:264/bitstring, 65:8, Pubkey3:520/bitstring, ?OP_3:8, ?OP_CHECKMULTISIG>>) ->
+parse_script(<<M:8, 65:8, Pubkey:65/binary, 33:8, Pubkey2:33/binary, 65:8, Pubkey3:65/binary, ?OP_3:8, ?OP_CHECKMULTISIG>>) ->
 	{multisig, {{norm(M),3}, [Pubkey, Pubkey2, Pubkey3]}};
 
-parse_script(<<M:8, 65:8, Pubkey:520/bitstring, 65:8, Pubkey2:520/bitstring, 33:8, Pubkey3:264/bitstring, ?OP_3:8, ?OP_CHECKMULTISIG>>) ->
+parse_script(<<M:8, 65:8, Pubkey:65/binary, 65:8, Pubkey2:65/binary, 33:8, Pubkey3:33/binary, ?OP_3:8, ?OP_CHECKMULTISIG>>) ->
 	{multisig, {{norm(M),3}, [Pubkey, Pubkey2, Pubkey3]}};
 
-parse_script(<<M:8, 33:8, Pubkey:264/bitstring, 65:8, Pubkey2:520/bitstring, 65:8, Pubkey3:520/bitstring, ?OP_3:8, ?OP_CHECKMULTISIG>>) ->
+parse_script(<<M:8, 33:8, Pubkey:33/binary, 65:8, Pubkey2:65/binary, 65:8, Pubkey3:65/binary, ?OP_3:8, ?OP_CHECKMULTISIG>>) ->
 	{multisig, {{norm(M),3}, [Pubkey, Pubkey2, Pubkey3]}};
 
-parse_script(<<M:8, 65:8, Pubkey:520/bitstring, 65:8, Pubkey2:520/bitstring, 65:8, Pubkey3:520/bitstring, ?OP_3:8, ?OP_CHECKMULTISIG>>) ->
+parse_script(<<M:8, 65:8, Pubkey:65/binary, 65:8, Pubkey2:65/binary, 65:8, Pubkey3:65/binary, ?OP_3:8, ?OP_CHECKMULTISIG>>) ->
 	{multisig, {{norm(M),3}, [Pubkey, Pubkey2, Pubkey3]}};
 
 % Open Assets Marker parsing
@@ -218,8 +218,7 @@ parse_oa_qlist(Q, B, Acc) ->
 
 parse_oa_metadata(B) ->
 	[MetaLength, R] = getVarInt(B),
-	Bits = MetaLength*8,
-	<<D:Bits/bitstring, _/binary>> = R,
+	<<D:MetaLength/binary, _/binary>> = R,
 	D.
 
 parse_script_2(B) ->
@@ -231,21 +230,20 @@ getPushData(<< ?OP_PUSHDATA2:8, TXCount:16/little, BinRest/binary >>) -> [TXCoun
 getPushData(<< ?OP_PUSHDATA4:8, TXCount:32/little, BinRest/binary >>) -> [TXCount, BinRest];
 getPushData(_) -> error.
 
-getVarInt(<< TXCount:8, BinRest/binary >>) when TXCount < 253 -> [TXCount, BinRest];
-getVarInt(<< 253:8, TXCount:16/little, BinRest/binary >>) -> [TXCount, BinRest];
-getVarInt(<< 254:8, TXCount:32/little, BinRest/binary >>) -> [TXCount, BinRest];
-getVarInt(<< 255:8, TXCount:64/little, BinRest/binary >>) -> [TXCount, BinRest];
+getVarInt(<<TXCount:8, BinRest/binary >>) when TXCount < 253 -> [TXCount, BinRest];
+getVarInt(<<253:8, TXCount:16/little, BinRest/binary >>) -> [TXCount, BinRest];
+getVarInt(<<254:8, TXCount:32/little, BinRest/binary >>) -> [TXCount, BinRest];
+getVarInt(<<255:8, TXCount:64/little, BinRest/binary >>) -> [TXCount, BinRest];
 getVarInt(_) -> error.
 
 getTxInputs(InputCount, Rest) -> getTxInputs(InputCount, Rest, []).
 getTxInputs(0, Rest, Acc) -> [Acc, Rest];
 getTxInputs(InputCount, Rest, Acc) ->
-	<< Txhash:256/bitstring,
+	<< Txhash:32/binary,
 	   TxIndex:32/little,
 	   BinRest/binary>> = Rest,
 	   [ScriptLength, TxRest] = getVarInt(BinRest),
-	   AdjustedScriptLength = ScriptLength*8,
-	   << Script:AdjustedScriptLength/bitstring, SeqNum:32/little, Next/binary >> = TxRest,
+	   << Script:ScriptLength/binary, SeqNum:32/little, Next/binary >> = TxRest,
 	   getTxInputs(InputCount-1, Next, [#btxin{txhash=binary:copy(Txhash),
 	   			                               txindex=TxIndex,
 	   			                               script=binary:copy(Script),
@@ -257,8 +255,7 @@ getTxOutputs(OutputCount, Rest, Acc, Index) ->
 	<< Value:64/little,
 	   BinRest/binary>> = Rest,
 	   [ScriptLength, TxRest] = getVarInt(BinRest),
-	   AdjustedScriptLength = ScriptLength*8,
-	   << Script:AdjustedScriptLength/bitstring, Next/binary >> = TxRest,
+	   << Script:ScriptLength/binary, Next/binary >> = TxRest,
 	   S = binary:copy(Script),
 	   ScriptExtendedInfo = parse_script(S),
 	   Address = lib_address:script_to_address(ScriptExtendedInfo, S),
@@ -295,16 +292,17 @@ extract(<<>>) -> ok;
 extract(<<MagicByte:32/little, 
     HeaderLength:32/little,
     VersionNumber:32/little, 
-    PreviousHash:256/bitstring, 
-    MerkleRoot:256/bitstring, 
+    PreviousHash:32/binary, 
+    MerkleRoot:32/binary, 
     TimeStamp:32/little, 
     TargetDifficulty:32/little, 
     Nonce:32/little,
-    BinRest/binary>>) ->
+    BinRest/binary>> = L) ->
+    try
     % Repack subset to create Block Hash
     HashBin = <<VersionNumber:32/little,
-                PreviousHash:256/bitstring,
-                MerkleRoot:256/bitstring,
+                PreviousHash:32/binary,
+                MerkleRoot:32/binary,
                 TimeStamp:32/little,
                 TargetDifficulty:32/little,
                 Nonce:32/little>>,
@@ -312,6 +310,7 @@ extract(<<MagicByte:32/little,
    [TXCount, Tbin] = getVarInt(BinRest),
     TxDataOffset = 88 + (byte_size(BinRest) - byte_size(Tbin)),
    [Tdata, Rest, TxOffsets] = getTransactions(TXCount, Tbin, TxDataOffset),
+   check_difficulty(hex:bin_reverse(BlockHash)),
    {ok, #bbdef{network=MagicByte,
    		       blockhash=binary:copy(BlockHash),
    		       headerlength=HeaderLength,
@@ -323,26 +322,30 @@ extract(<<MagicByte:32/little,
    		       nonce=Nonce,
    		       txcount=TXCount,
 			   txdata=lists:reverse(Tdata)},
-	lists:reverse(TxOffsets), Rest};
- extract(<<R:8, _Bin/binary>>) when R > 0 ->
-	io:format("Problem: ~w~n", [binary:bin_to_list(_Bin, {0, 10})]),
-	{scan, _Bin};
+	lists:reverse(TxOffsets), Rest}
+	catch _:_ ->  {scan, L}
+	end;
 extract(Data) -> {scan, Data}.
 
+%% This isn't a real difficulty check
+%% it's here to filter out crap data that results in
+%% partial writes of bad blocks onto disk
+check_difficulty(<<0:32, _/binary>>) -> ok;
+check_difficulty(_) -> throw(difficulty_check_failure).
 
 extract_header(<<MagicByte:32/little, 
     HeaderLength:32/little,
     VersionNumber:32/little, 
-    PreviousHash:256/bitstring, 
-    MerkleRoot:256/bitstring, 
+    PreviousHash:32/binary, 
+    MerkleRoot:32/binary, 
     TimeStamp:32/little, 
     TargetDifficulty:32/little, 
     Nonce:32/little,
     BinRest/binary>>) ->
     % Repack subset to create Block Hash
     HashBin = <<VersionNumber:32/little,
-                PreviousHash:256/bitstring,
-                MerkleRoot:256/bitstring,
+                PreviousHash:32/binary,
+                MerkleRoot:32/binary,
                 TimeStamp:32/little,
                 TargetDifficulty:32/little,
                 Nonce:32/little>>,
